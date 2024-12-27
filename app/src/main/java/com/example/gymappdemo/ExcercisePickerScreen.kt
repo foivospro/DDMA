84e641986c930fce3d410d5b01b9bb3eec2e37df
Giannis Karyotakis <110292528+karyotakisg@users.noreply.github.com>
2024-12-28 01:45:43 +0200

    Impove Excercise Picker Screen

--- app/src/main/java/com/example/gymappdemo/ExcercisePickerScreen.kt
+++ app/src/main/java/com/example/gymappdemo/ExcercisePickerScreen.kt
@@ -1,38 +1,33 @@
 package com.example.gymappdemo
 
-import android.content.res.Configuration
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.ExperimentalAnimationApi
-import androidx.compose.animation.core.MutableTransitionState
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.spring
-import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.animation.slideInVertically
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.sizeIn
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.sizeIn
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.input.rememberTextFieldState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.MoreVert
@@ -41,31 +36,26 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FilterChip
+import androidx.compose.material3.FilterChipDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.ListItem
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SearchBar
-import androidx.compose.material3.SearchBarDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.semantics.isTraversalGroup
-import androidx.compose.ui.semantics.semantics
-import androidx.compose.ui.semantics.traversalIndex
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.gymappdemo.sampledata.DataSource
 import com.example.gymappdemo.sampledata.Excercise
 import com.example.gymappdemo.ui.theme.GymAppDemoTheme
 
@@ -78,7 +68,7 @@ fun ExcercisesList(
 ) {
     var query by rememberSaveable { mutableStateOf("") }
     var active by rememberSaveable { mutableStateOf(false) }
-
+    val muscleGroups = listOf("Chest", "Legs", "Arms")
     Scaffold(
         bottomBar = { BottomNavigationBar() }
     ) { scaffoldPadding ->
@@ -120,7 +110,39 @@ fun ExcercisesList(
                     }
                 }
             }
-
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
+                    .align(Alignment.CenterHorizontally),
+                horizontalArrangement = Arrangement.Center
+            ) {
+                for (muscleGroup in muscleGroups) {
+                    FilterChip(
+                        onClick = { /*TODO*/ },
+                        label = { Text(muscleGroup) },
+                        selected = false,
+                        modifier = Modifier
+                            .padding(start = 16.dp),
+                        leadingIcon = {
+                            Icon(
+                                painter = painterResource(R.drawable.filter_alt),
+                                contentDescription = null
+                            )
+                        },
+                        shape = RoundedCornerShape(50), // Pill-shaped design
+                        colors = FilterChipDefaults.filterChipColors(
+                            containerColor = MaterialTheme.colorScheme.onSecondary, // Default background
+                            selectedContainerColor = MaterialTheme.colorScheme.primaryContainer,
+                        ),
+                        border = FilterChipDefaults.filterChipBorder(
+                            borderColor = MaterialTheme.colorScheme.outline,
+                            selectedBorderColor = MaterialTheme.colorScheme.primary,
+                            disabledBorderColor = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.25f)
+                    )
+                    )
+                }
+            }
             // Filtered Exercise List
             val filteredExercises = if (query.isNotEmpty()) {
                 excercises.filter {
@@ -190,6 +212,31 @@ fun ExerciseCard(
                 .padding(16.dp)
                 .sizeIn(minHeight = 72.dp)
         ) {
+
+            // Image and Button Column
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.SpaceBetween,
+                modifier = Modifier.fillMaxHeight()
+            ) {
+                Icon(
+                    painter = painterResource(iconResource),
+                    contentDescription = stringResource(name),
+                    modifier = Modifier
+                        .size(56.dp) // Adjust size for better fit
+                        .clip(RoundedCornerShape(8.dp)),
+                    tint = MaterialTheme.colorScheme.primary
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+                Button(
+                    onClick = onStartClick,
+                    modifier = Modifier
+                        .sizeIn(minWidth = 80.dp, minHeight = 24.dp)
+                ) {
+                    Text("Add", style = MaterialTheme.typography.labelSmall)
+                }
+            }
+            Spacer(modifier = Modifier.width(40.dp))
             // Text Column
             Column(
                 modifier = Modifier.weight(1f),
@@ -203,33 +250,6 @@ fun ExerciseCard(
                     text = stringResource(descriptionRes),
                     style = MaterialTheme.typography.bodyLarge
                 )
-            }
-
-            Spacer(Modifier.width(16.dp))
-
-            // Image and Button Column
-            Column(
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.SpaceBetween,
-                modifier = Modifier.fillMaxHeight()
-            ) {
-                Image(
-                    painter = painterResource(iconResource),
-                    contentDescription = stringResource(name),
-                    alignment = Alignment.Center,
-                    contentScale = ContentScale.Crop,
-                    modifier = Modifier
-                        .size(56.dp) // Adjust size for better fit
-                        .clip(RoundedCornerShape(8.dp))
-                )
-                Spacer(modifier = Modifier.height(8.dp))
-                Button(
-                    onClick = onStartClick,
-                    modifier = Modifier
-                        .sizeIn(minWidth = 80.dp, minHeight = 24.dp)
-                ) {
-                    Text("Add", style = MaterialTheme.typography.labelSmall)
-                }
             }
         }
     }
